207a5de4fcdd42860f5a64c7e9e27c6929825afd
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2013-08-18 22:23:25 +0300

    Rename 'diffs' to 'patches' in pronto/rubocop_spec

--- spec/pronto/rubocop_spec.rb
+++ spec/pronto/rubocop_spec.rb
@@ -5,15 +5,15 @@ module Pronto
     let(:rubocop) { Rubocop.new }
 
     describe '#run' do
-      subject { rubocop.run(diffs) }
+      subject { rubocop.run(patches) }
 
-      context 'diffs are nil' do
-        let(:diffs) { nil }
+      context 'patches are nil' do
+        let(:patches) { nil }
         it { should == [] }
       end
 
-      context 'no diffs' do
-        let(:diffs) { [] }
+      context 'no patches' do
+        let(:patches) { [] }
         it { should == [] }
       end
 
@@ -21,7 +21,7 @@ module Pronto
         let(:path_to_repo) { File.join(File.dirname(__FILE__), '../../') }
         let(:repo) { Rugged::Repository.new(path_to_repo) }
 
-        let(:diffs) { repo.diff('f8d5f2c', '504469e') }
+        let(:patches) { repo.diff('f8d5f2c', '504469e') }
 
         its(:count) { should == 3 }
         its(:'first.count') { should == 1 }
