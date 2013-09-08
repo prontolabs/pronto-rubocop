bc223d60aa4ef0a0a65fcffaa68135a7cdf49ab8
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2013-09-08 11:59:50 +0300

    Refactor runner further

--- lib/pronto/rubocop.rb
+++ lib/pronto/rubocop.rb
@@ -11,24 +11,18 @@ module Pronto
       return [] unless patches
 
       patches.select { |patch| patch.additions > 0 }
+             .select { |patch| ruby_file?(patch.delta.new_file_full_path) }
              .map { |patch| inspect(patch) }
              .flatten.compact
     end
 
     def inspect(patch)
-      path = patch.delta.new_file_full_path
+      offences = @cli.inspect_file(patch.delta.new_file_full_path)
 
-      if ruby_file?(path)
-        offences = @cli.inspect_file(path)
-        messages_from(offences, patch)
-      end
-    end
-
-    def messages_from(offences, patch)
       offences.map do |offence|
         patch.added_lines.select { |line| line.new_lineno == offence.line }
                          .map { |line| new_message(patch, offence, line) }
-      end.flatten.compact
+      end
     end
 
     def new_message(patch, offence, line)
