d2f8c3b828b79c4814e1f6a8d0d581fa72ed9c64
Mindaugas Mozūras <mindaugas.mozuras@gmail.com>
2015-09-06 22:08:45 +0300

    Explicitly reject offenses disabled by rubocop:disable comment
    Fixes #13

--- lib/pronto/rubocop.rb
+++ lib/pronto/rubocop.rb
@@ -24,7 +24,7 @@ module Pronto
       processed_source = ::RuboCop::ProcessedSource.from_file(patch.new_file_full_path.to_s)
       offences = @inspector.send(:inspect_file, processed_source).first
 
-      offences.map do |offence|
+      offences.sort.reject(&:disabled?).map do |offence|
         patch.added_lines.select { |line| line.new_lineno == offence.line }
                          .map { |line| new_message(offence, line) }
       end
